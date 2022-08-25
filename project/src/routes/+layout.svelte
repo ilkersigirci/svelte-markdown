8d6fe0703f1b18964914338eac6196bf5436901f
İlker SIĞIRCI <39417966+ilkersigirci@users.noreply.github.com>
2022-08-25 22:14:45 +0000

    before markdown setup

---
+++ project/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+import Header from "$lib/components/Header.svelte"
+import '$lib/styles/style.scss'
+</script>
+
+<Header/>
+
+<main>
+  <slot />
+</main>
+
+<footer>Hello, I'm the footer.</footer>
