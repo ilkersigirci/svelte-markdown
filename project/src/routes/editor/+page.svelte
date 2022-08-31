9a05b15b04167751bbbdaecab910a6c36f288ecc
İlker SIĞIRCI <39417966+ilkersigirci@users.noreply.github.com>
2022-08-31 10:19:53 +0000

    bytemd editor added

---
+++ project/src/routes/editor/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Editor } from 'bytemd';
+	import gfm from '@bytemd/plugin-gfm';
+	import 'bytemd/dist/index.css';
+
+	let value = '';
+	let plugins = [gfm()];
+</script>
+
+<div class="container">
+	<Editor
+		{value}
+		on:change={(e) => {
+			value = e.detail.value;
+		}}
+		{plugins}
+	/>
+	<p>Raw input</p>
+	<p>{value}</p>
+</div>
+
+<style>
+	.container {
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+</style>
